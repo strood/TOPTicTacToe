fd24d2fca7da6407c40a35403dc6484468a31134
strood <stroodz19@gmail.com>
2020-08-28 11:46:42 -0600

    Player select style and function

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //  Gameboard holds the board as an array, and functions to interact with board
-// Built as a module (Revealing module pattern) -- Scratch that
+// Built as a module (Revealing module pattern)
 const gameBoard = (() => {
   // Default board on setup, false status signifying incomplete game, winner defaulted
   const board = Array(9).fill("");
@@ -81,23 +81,20 @@ const gameBoard = (() => {
   };
 })();
 
-// Players will be stored in objects, include player functionality
-// Add functions to allow players to mark a specific spot on board and tie to DOM
-// allowing for "click to play" interaction
-const Player = (name, token, human) => {
+// Players will be stored in objects
+const playerFactory = (name, token, ai) => {
   // Check if human, will setup differently if AI
-  human = human || true;
+  let bot = ai || false;
   const getName = () => name;
-  const isHuman = () => human;
   const getToken = () => token;
+  const isBot = () => bot;
 
-  return {getName, isHuman, getToken}
+  return {getName, getToken, isBot}
 }
 
 
 
-// Object to control flow of game
-// MAY MAKE INTO MODULE AS WE ONLY NEED ONE
+// Object to hold game details
 const Game = (player1, player2, board) => {
   // Returns  a new game object to be played with given chars and new board
   // Result will be false to start, change to winner/ tie based on outcome
@@ -114,42 +111,135 @@ const Game = (player1, player2, board) => {
 const displayController = ((game) => {
 
   const renderPlayerSelection = () => {
-    let body = document.querySelector('body');;
-    let boardBox = document.createElement('div');
-    boardBox.setAttribute('class', 'boardBox');
-    console.log(boardBox)
-    body.insertBefore(boardBox, body.children[0]);
+
+    let body = document.querySelector('body');
+
+    let selectBox = document.createElement('div');
+    selectBox.setAttribute('class', 'selectBox');
+    let selectTitle = document.createElement('h1');
+    selectTitle.textContent = "Player Names"
+
+    let p1Box = document.createElement('div');
+    p1Box.setAttribute('class', 'playerBox');
+
+    let p2Box = document.createElement('div');
+    p2Box.setAttribute('class', 'playerBox');
+
+    const p1NameInput = document.createElement('input');
+    p1NameInput.setAttribute('class','nameInput');
+    p1NameInput.setAttribute('data-player','1');
+    p1NameInput.type = "name";
+    p1NameInput.maxLength = 15;
+    let p1AiInput = document.createElement('input');
+    p1AiInput.type = "checkbox";
+    let p1NameLabel = document.createElement('label');
+    p1NameLabel.textContent = "Player 1:"
+    let p1AiLabel = document.createElement('label');
+    p1AiLabel.textContent = "- AI?"
+
+    const p2NameInput = document.createElement('input');
+    p2NameInput.setAttribute('class','nameInput');
+    p2NameInput.setAttribute('data-player','2');
+    p2NameInput.type = "name";
+    p2NameInput.maxLength = 15;
+    let p2AiInput = document.createElement('input');
+    p2AiInput.type = "checkbox";
+    let p2NameLabel = document.createElement('label');
+    p2NameLabel.textContent = "Player 2:"
+    let p2AiLabel = document.createElement('label');
+    p2AiLabel.textContent = "- AI?"
+
+    let nameButton = document.createElement('button')
+    nameButton.textContent = "- Play Game -"
+    let startGameFunct = () => {
+      // Create players from info on page, then remove playerSelect
+
+      let player1 = playerFactory(p1NameInput.value, "X", (p1AiInput.checked) ? true : false);
+      let player2 = playerFactory(p2NameInput.value, "O", (p2AiInput.checked) ? true : false);
+      console.log(player1);
+      console.log(typeof(player2));
+      console.log(player1.getName());
+      console.log(player2.getName());
+      console.log(player1.getToken());
+      console.log(player2.getToken());
+      console.log(player1.isBot());
+      console.log(player2.isBot());
+
+      // Setup new game
+      let game = Game(player1, player2, gameBoard);
+
+      // Clear page
+      body.removeChild(selectBox);
+
+      // Pass new game to board render
+      renderBoard(game);
+    }
+    nameButton.addEventListener('click', startGameFunct)
+
+
+    p1Box.appendChild(p1NameInput);
+    p1Box.insertBefore(p1NameLabel, p1NameInput);
+    p1NameInput.insertAdjacentElement("afterend", p1AiInput);
+    p1AiInput.insertAdjacentElement("beforebegin", p1AiLabel);
+
+
+    p2Box.appendChild(p2NameInput);
+    p2Box.insertBefore(p2NameLabel, p2NameInput);
+    p2NameInput.insertAdjacentElement("afterend", p2AiInput);
+    p2AiInput.insertAdjacentElement("beforebegin", p2AiLabel);
+
+    selectBox.appendChild(selectTitle);
+    selectBox.appendChild(p1Box);
+    selectBox.appendChild(p2Box);
+    selectBox.appendChild(nameButton);
+
+    console.log(selectBox);
+    body.insertBefore(selectBox, body.children[0]);
   }
 
   const renderWelcome = () => {
     // Build element
     let title1 = document.createElement('h1');
     title1.textContent = "Welcome";
+
     let title2 = document.createElement('h2');
     title2.textContent = "to"
+
     let title3 = document.createElement('h1');
     title3.textContent = "Tic-Tac-Toe"
+
+    let clickText = document.createElement('p');
+    clickText.textContent = "- Click Anywhere to Continue -"
+
     let titleDiv = document.createElement('div');
     titleDiv.setAttribute('class', 'titleDiv')
     titleDiv.appendChild(title1);
     titleDiv.appendChild(title2);
     titleDiv.appendChild(title3);
+    titleDiv.appendChild(clickText);
+
     let body = document.querySelector('body');
     body.appendChild(titleDiv)
-    let html = document.querySelector('html');
+
     // Add element with one time event listener on html
+    let html = document.querySelector('html');
     let setupFunct = () => {
       body.removeChild(titleDiv)
       html.removeEventListener('click', setupFunct)
       renderPlayerSelection()
     }
     html.addEventListener('click', setupFunct);
+
   }
 
   // render board in board display area
   const renderBoard = (game) => {
     // Grab board box
-    let boardBox = document.querySelector(".boardBox");
+
+    let boardBox = document.createElement("div");
+    boardBox.setAttribute('class', 'boardBox');
+    let body = document.querySelector('body');
+    body.insertAdjacentElement("afterbegin", boardBox);
 
     // Loop through our boards array and set up spots accordingly
     // Go backwards so we can push appends out to end
@@ -185,10 +275,10 @@ const displayController = ((game) => {
               spot.textContent = game.currentPlayer.getToken
               if (game.currentPlayer == game.player1) {
                 game.currentPlayer = game.player2;
-                displayController.reRenderBoard(game)
+                reRenderBoard(game)
               } else {
                 game.currentPlayer = game.player1;
-                displayController.reRenderBoard(game)
+                reRenderBoard(game)
               }
             } else {
               console.log("Invalid move")
@@ -218,6 +308,7 @@ const displayController = ((game) => {
 
   return {
     renderWelcome,
+    renderBoard,
   };
 })();
 
@@ -232,19 +323,26 @@ const playRound = () => {
 
 }
 
+
+
+
+
+
+
+
 // Game setup for testing
 console.log(gameBoard.getBoard());
 console.log(gameBoard);
 gameBoard.getBoard();
 
-const myPlayer1 = Player("Joe", "X");
+const myPlayer1 = playerFactory("Joe", "X");
 console.log(myPlayer1.getName());
-console.log(myPlayer1.isHuman());
+console.log(myPlayer1.isBot());
 console.log(myPlayer1);
 
-const myPlayer2 = Player("Jeb", "O");
+const myPlayer2 = playerFactory("Jeb", "O");
 console.log(myPlayer2.getName());
-console.log(myPlayer2.isHuman());
+console.log(myPlayer2.isBot());
 console.log(myPlayer2);
 
 const newGame = Game(myPlayer1, myPlayer2, gameBoard)
@@ -255,7 +353,7 @@ const newGame = Game(myPlayer1, myPlayer2, gameBoard)
 // newGame.board.playMove(myPlayer2, 6);
 
 console.log(gameBoard.getBoard());
-// displayController.renderBoard(newGame)
+// displayController.renderBoard(newGame);
 
 // On load enter in with welcome.
 document.onload = displayController.renderWelcome()
